6dfe2bc30d7cf94a74ea579382dc9bf9ddaffee5
Shreyash kumar pandey <Shreyash.pandey.katni@gmail.com>
2022-01-02 18:33:19 +0530

    "solved LongestPalindromicSubstring"

--- longestPalindromicSubstring.cpp
+++ longestPalindromicSubstring.cpp
@@ -2,15 +2,29 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
+bool isPalindrome(string s)
+{
+    int l = 0, r = s.size() - 1;
+    while (l < r)
+    {
+        if (s[l] != s[r])
+            return false;
+        l++;
+        r--;
+    }
+    return true;
+}
 string longestPalindrome(string s)
 {
+    if (isPalindrome(s))
+        return s;
     int n = s.size();
     bool dp[n][n];
-    memset(dp,0,sizeof(dp));
-    int start = 0, end = 0;
+    memset(dp, 0, sizeof(dp));
+    int start = 0;
     int max_len = 1;
     for (int i = 0; i < n; i++)
     {
@@ -22,33 +36,33 @@ string longestPalindrome(string s)
         {
             dp[i][i + 1] = true;
             start = i;
-            end = i + 1;
+            // end = i + 1;
             max_len = 2;
         }
     }
-    for (int i = 3; i <= n; i++)
+    for (int k = 3; k <= n; ++k)
     {
-        for (int j = 0; j < n - i + 1; j++)
+        for (int i = 0; i < n - k + 1; ++i)
         {
-            int k = i + j - 1;
-            string sub = s.substr(j, i);
-            if (dp[j + 1][k - 1] && s[j] == s[k])
+            int j = i + k - 1;
+            string sub = s.substr(i, j);
+            if (dp[i + 1][j - 1] && s[i] == s[j])
             {
-                dp[i][k] = true;
-                if (i > max_len)
+                dp[i][j] = true;
+                if (k > max_len)
                 {
-                    max_len = i;
-                    start = j;
-                    end = k;
+                    max_len = k;
+                    start = i;
+                    // end = k;
                 }
             }
         }
     }
-    return s.substr(start, end - start + 1);
+    return s.substr(start, max_len);
 }
 int main(int argc, char const *argv[])
 {
-    string s = "babad";
+    string s = "bacabab";
     cout << longestPalindrome(s) << endl;
     return 0;
 }
--- minimumPointsToReachDestination.cpp
+++ minimumPointsToReachDestination.cpp
@@ -7,27 +7,24 @@ using namespace std;
 int minPoints(vector<vector<int>> points, int M, int N)
 {
     int dp[M][N] = {0};
-    for(int i=0;i<M;i++)
-    {
-        for(int j=0;j<N;j++)
-        {
-            if(i==0 && j==0)
-            {
-                dp[i][j] = -1+points[i][j];
-            }
-            else if(i==0)
-            {
-                dp[i][j] = dp[i][j-1] + points[i][j];
-            }
-            else if(j==0)
-            {
-                dp[i][j] = dp[i-1][j] + points[i][j];
-            }
-            else
-            {
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1]) + points[i][j];
-            }
+    dp[M-1][N-1] = points[M-1][N-1] >0 ? 1 : abs(points[M-1][N-1])+1;
+    for(int i=M-2;i>=0;i--){
+        dp[i][N-1] = max(dp[i+1][N-1] - points[i][N-1],1);
+    }
+    for(int i=N-2;i>=0;i--){
+        dp[M-1][i] = max(dp[M-1][i+1] - points[M-1][i] ,1);
+    }
+    for(int i=M-2;i>=0;i--){
+        for(int j=N-2;j>=0;j--){
+            dp[i][j] = max(min(dp[i+1][j], dp[i][j+1])-points[i][j],1);
         }
     }
-    return dp[M-1][N-1];
+    return dp[0][0];
+}
+int main(int argc, char const *argv[])
+{
+    int M = 3, N = 3;
+    vector<vector<int>> points = {{-2,-3,3},{-5,-10,1},{10,30,-5}};
+    cout<<minPoints(points,M,N)<<endl;
+    return 0;
 }
