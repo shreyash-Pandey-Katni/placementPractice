ee2d8c80265adf9e3e4e8be596987dbc2db4b63a
Shreyash kumar pandey <Shreyash.pandey.katni@gmail.com>
2022-01-04 20:22:14 +0530

    Minimum Distance Between Two Numbers

--- minimumDistanceBetweenTwoNumbers.cpp
+++ minimumDistanceBetweenTwoNumbers.cpp
@@ -1,20 +1,35 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<bits/stdc++.h>
-#include<unordered_map>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
-int minDist(int a[], int n, int x, int y){
-    int indexX = -1;
-    int indexY = -1;
-    for (int i = 0; i < n && (indexX==-1 || indexY == -1); i++)
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+int minDist(int a[], int n, int x, int y)
+{
+    int prev = -1;
+    int minDist = INT_MAX;
+    for (int i = 0; i < n; i++)
     {
-        if(a[i] == x){
-            indexX = i;
-        }
-        if(a[i] == y){
-            indexY = i;
+        if (a[i] == x || a[i] == y)
+        {
+            if (prev != -1 && a[i] != a[prev])
+            {
+                minDist = min(minDist, i - prev);
+            }
+            prev = i;
         }
     }
-    return abs(indexX-indexY);
+    return minDist == INT_MAX ? -1 : minDist;
+}
+int main(int argc, char const *argv[])
+{
+    int a[] = {1, 2, 3, 2};
+    int n = sizeof(a) / sizeof(a[0]);
+    int x = 1, y = 2;
+    cout << minDist(a, n, x, y) << endl;
+    return 0;
 }
